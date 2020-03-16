d0a75dccd2486d79b753486e14f1011acfe868cb
kot.yuhara <kot.yuhara@trifort.jp>
2020-03-16 16:26:22 +0900

    catboost tune parameter

--- optcat/core.py
+++ optcat/core.py
@@ -1,5 +1,5 @@
 import copy
-from typing import Dict, Any, List, Callable, Optional, Union, Iterable
+from typing import Dict, Any, Callable, Optional, Union, Iterable
 
 import catboost as cb
 from optuna import distributions
@@ -17,12 +17,8 @@ class _Objective:
         eval_name: str,
         is_higher_better: bool,
         n_samples: int,
-        callbacks: Optional[List[Callable]] = None,
-        categorical_feature: Union[List[int], List[str], str] = "auto",
         cv: Optional[CVType] = None,
         early_stopping_rounds: Optional[int] = None,
-        enable_pruning: bool = False,
-        feature_name: Union[List[str], str] = "auto",
         f_eval: Optional[Callable] = None,
         f_obj: Optional[Callable] = None,
         n_estimators: int = 100,
@@ -30,14 +26,10 @@ class _Objective:
             Dict[str, distributions.BaseDistribution]
         ] = None,
     ) -> None:
-        self.callbacks = callbacks
-        self.categorical_feature = categorical_feature
         self.cv = cv
         self.dataset = dataset
         self.early_stopping_rounds = early_stopping_rounds
-        self.enable_pruning = enable_pruning
         self.eval_name = eval_name
-        self.feature_name = feature_name
         self.feval = f_eval
         self.fobj = f_obj
         self.is_higher_better = is_higher_better
@@ -64,8 +56,8 @@ class _Objective:
         params = self.params.copy()  # type: Dict[str, Any]
 
         if self.param_distributions is None:
-            params["feature_fraction"] = trial.suggest_discrete_uniform(
-                "feature_fraction", 0.1, 1.0, 0.05
+            params["colsample_bylevel"] = trial.suggest_discrete_uniform(
+                "colsample_bylevel", 0.1, 1.0, 0.05
             )
             params["max_depth"] = trial.suggest_int("max_depth", 1, 7)
             params["num_leaves"] = trial.suggest_int(
@@ -77,19 +69,18 @@ class _Objective:
                 1,
                 max(1, int(self.n_samples / params["num_leaves"])),
             )
-            params["lambda_l1"] = trial.suggest_loguniform(
-                "lambda_l1", 1e-09, 10.0
-            )
-            params["lambda_l2"] = trial.suggest_loguniform(
+            params["l2_leaf_reg"] = trial.suggest_loguniform(
                 "lambda_l2", 1e-09, 10.0
             )
 
-            if params["boosting_type"] != "goss":
-                params["bagging_fraction"] = trial.suggest_discrete_uniform(
-                    "bagging_fraction", 0.5, 0.95, 0.05
+            if params["boosting_type"] == "Bayesian":
+                params["bagging_temperature"] = trial.suggest_discrete_uniform(
+                    "bagging_temperature", 0.5, 0.95, 0.05
                 )
-                params["bagging_freq"] = trial.suggest_int(
-                    "bagging_freq", 1, 10
+            elif params["boosting_type"] == "Bernoulli" or \
+                params["boosting_type"] == "Poisson":
+                params["subsample"] = trial.suggest_uniform(
+                    "subsample", 0.1, 1
                 )
 
             return params
