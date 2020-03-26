7dabbf44267df1f75b4a114f5dbd3d749dcd1ea5
kot.yuhara <kot.yuhara@trifort.jp>
2020-03-26 15:42:39 +0900

    compare origin score to tuner score

--- tests/test_core.py
+++ tests/test_core.py
@@ -1,10 +1,27 @@
+import catboost as cb
 import pytest
 from sklearn import datasets
+from sklearn.model_selection import train_test_split
 
 from optcat.core import CatBoostClassifier, CatBoostRegressor
 
 N_TRIALS = 2
 ITERATIONS = 10
+RANDOM_STATE = 41
+SEED = 42
+
+
+def test_predict():
+    params = {
+        "bootstrap_type": "Bayesian",
+        "loss_function": "Logloss",
+        "iterations": 1,
+    }
+    model = CatBoostClassifier(params=params, n_trials=1)
+    data, target = datasets.load_breast_cancer(return_X_y=True)
+    model.fit(X=data, y=target)
+    y_pred = model.predict(data)
+    assert target.shape == y_pred.shape
 
 
 def test_classifier_fit():
@@ -48,3 +65,24 @@ def test_regressor_fit():
     model = CatBoostRegressor(params=params, n_trials=N_TRIALS)
     data, target = datasets.load_boston(return_X_y=True)
     model.fit(X=data, y=target)
+
+
+def test_score():
+    params = {
+        "bootstrap_type": "Bayesian",
+        "loss_function": "Logloss",
+        "iterations": ITERATIONS,
+    }
+    data, target = datasets.load_breast_cancer(return_X_y=True)
+    x_train, x_test, y_train, y_test = train_test_split(
+        data, target, random_state=RANDOM_STATE
+    )
+
+    def get_score(_model):
+        _model.fit(X=x_train, y=y_train)
+        return _model.score(x_test, y_test)
+
+    origin_score = get_score(cb.CatBoostClassifier(**params))
+    tuner_score = get_score(CatBoostClassifier(params))
+
+    assert origin_score <= tuner_score
